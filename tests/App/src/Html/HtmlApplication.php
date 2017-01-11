8d15daddc76aa4dd6c707a3d4648913c52212822
Cheren <kalistratov.s.m@gmail.com>
2017-01-12 00:18:50 +0300

    Set static method for test

--- tests/App/src/Html/HtmlApplication.php
+++ tests/App/src/Html/HtmlApplication.php
@@ -26,7 +26,7 @@ class HtmlApplication
     /**
      * @return string
      */
-    public function getFunctionName()
+    public static function getFunctionName()
     {
         return __FUNCTION__;
     }
