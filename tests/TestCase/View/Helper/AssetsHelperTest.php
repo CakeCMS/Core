0398d3b45b26d4b03b63cba02ec6f7569092de92
Cheren <kalistratov.s.m@gmail.com>
2017-03-27 01:44:52 +0400

    Dump test files for travis

--- tests/TestCase/View/Helper/AssetsHelperTest.php
+++ tests/TestCase/View/Helper/AssetsHelperTest.php
@@ -98,6 +98,10 @@ class AssetsHelperTest extends HelperTestCase
         $jquery   = $this->_helper()->getAssets('script.jquery.output');
         $fancybox = $this->_helper()->getAssets('script.fancybox.output');
 
+        dump($jquery);
+dump($fancybox);
+dump($this->_helper()->getAssets('script'));
+
         $this->assertHtml($expected['jquery'], $jquery);
         $this->assertHtml($expected['fancybox'], $fancybox);
 
